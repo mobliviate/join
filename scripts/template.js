9498bef50b3629a9dd791285cbf574b5dcb2b07f
kevinkaroly <kevinkaroly@gmx.at>
2025-04-17 21:08:36 +0200

    changed directory name from script to scripts

---
+++ scripts/template.js
@@ -0,0 +1,33 @@
+function getBodyTemplate() {
+  return `
+    <div class="main-div">
+      <div class="left">
+        <div class="sidebar-logo">
+          <img src="assets/svg/join-logo.svg" alt="Join Logo">
+        </div>
+        <nav class="sidebar-nav">
+          <a href="index.html" class="sidebar-link" data-page="index.html">
+            <img src="assets/svg/summary-icon.svg"> Summary
+          </a>
+          <a href="add_task.html" class="sidebar-link" data-page="add_task.html">
+            <img src="assets/svg/edit-icon.svg"> Add Task
+          </a>
+          <a href="board.html" class="sidebar-link" data-page="board.html">
+            <img src="assets/svg/board-icon.svg"> Board
+          </a>
+          <a href="contact.html" class="sidebar-link" data-page="contact.html">
+            <img src="assets/svg/contact-icon.svg"> Contacts
+          </a>
+        </nav>
+        <div class="sidebar-footer">
+          <a href="#">Privacy Policy</a>
+          <a href="legal_notice.html">Legal notice</a>
+        </div>
+      </div>
+      <div class="right">
+        <header class="header" id="header"></header>
+        <main class="main" id="main"></main>
+      </div>
+    </div>
+  `;
+}
