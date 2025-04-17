8d946522b38af499d642aaf2a840b808d0cd78ce
Kevin Karoly <85257127+kevinkaroly@users.noreply.github.com>
2025-04-17 21:09:14 +0200

    Delete script directory

--- script/header.js
+++
@@ -1,5 +0,0 @@
-async function loadHeader() {
-  const response = await fetch("header.html");
-  const headerHTML = await response.text();
-  document.getElementById("header").innerHTML = headerHTML;
-}
