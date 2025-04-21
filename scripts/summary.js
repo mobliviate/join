c5981d5605c556f84e85e9a0906bb33c24b434d2
Veli <s.velicode@gmail.com>
2025-04-21 13:41:45 +0200

    Implement getSummerySectionTemplate function and update main content in initSummary

--- scripts/summary.js
+++ scripts/summary.js
@@ -2,6 +2,16 @@ function initSummary() {
     document.body.innerHTML = getBodyTemplate();
     loadHeader();
     highlightActiveSidebarLink();
-    document.getElementById("main").innerHTML = "Test";
-    // getContactsSectionTemplate();
+    document.getElementById("main").innerHTML = getSummerySectionTemplate();
+  }
+
+  function getSummerySectionTemplate() {
+    return `
+        <section class="summary_container">
+            <div class="summary_wrapper">
+                <div class="summary_header"></div>
+                <div class="summary_content"></div>
+            </div>
+        </section>
+    `;
   }
