13df57b4e99b0c2724a77f0d240b05f11e30a4d1
Veli <s.velicode@gmail.com>
2025-04-26 11:10:38 +0200

    Add initial summary data structure to summary_test_db.js

---
+++ scripts/summary_test_db.js
@@ -0,0 +1,14 @@
+{
+    "summary": {
+        "todo": 1,
+        "done": 1,
+        "priority": {
+            "amount": 1,
+            "status": "Urgent",
+            "deadline": "October 16, 2022"
+        },
+        "tasksInBoard": 5,
+        "tasksInProgress": 2,
+        "awatingFeedback": 2
+    }
+}
