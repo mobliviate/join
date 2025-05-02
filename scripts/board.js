36567e1ea5c09a00c17fe405fc2d32b44046f7e7
Gyözö Pere <pere_gyozo@freemail.com>
2025-05-03 00:12:11 +0200

    Render Tasks Basic Function

--- scripts/board.js
+++ scripts/board.js
@@ -1,6 +1,22 @@
+const tasksUrl = "https://join-bc74a-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
+
+
 function initBoard() {
-    document.body.innerHTML = getBodyTemplate();
+  document.body.innerHTML = getBodyTemplate();
   loadHeader();
   highlightActiveSidebarLink();
   document.getElementById("main").innerHTML = getBoardTemplate();
+  renderTasks();
+}
+
+async function renderTasks() {
+  let taskRefUrl = await fetch(tasksUrl);
+  let taskRefUrlToJson = await taskRefUrl.json();
+  for (let indexTask = 0; indexTask < taskRefUrlToJson.length; indexTask++) {
+    let taskRef = taskRefUrlToJson[indexTask]
+    let taskContentRef = document.getElementById(`${taskRef.status}`);
+    taskContentRef.innerHTML += getRenderTask(taskRef,indexTask);
+    //renderSubtasks(taskRef);
+    //renderInitials(taskRef);
+  }
 }
--- scripts/board_template.js
+++ scripts/board_template.js
@@ -30,11 +30,29 @@ function getBoardTemplate() {
             </div>
           </div>
           <div class="column-divs">
-            <div id="0" class="columns"></div>
-            <div id="1" class="columns"></div>
-            <div id="2" class="columns"></div>
-            <div id="3" class="columns"></div>
+            <div id="todo" class="columns"></div>
+            <div id="progress" class="columns"></div>
+            <div id="feedback" class="columns"></div>
+            <div id="done" class="columns"></div>
           </div>
         </div>
     `;
+}
+
+function getRenderTask(task,indexTask) {
+  return`
+    <div>
+      <div class="category_board">${task.category}</div>
+      <div class="title_board">${task.title}</div>
+      <div class="description_board">${task.description}</div>
+      <div class="subtasks_board">
+        <div id="progresbar${indexTask}"></div>
+        <p id="subtask${indexTask}"></p>
+      </div>
+      <div class="initials_priority_board">
+        <div class="initials_board" id="initials${indexTask}"></div>
+        <img class="priority_board" src="./assets/svg/prio_${task.priority}.svg" alt="${task.priority}_icon">
+      </div>
+    </div>
+  `;
 }
