bd044f40423fb9cbfb761d21487d9a9ecf3f5b11
xschlexx <alexbysmith@gmail.com>
2025-06-04 10:02:02 +0200

    bugfix board console error

--- scripts/board_template.js
+++ scripts/board_template.js
@@ -558,19 +558,19 @@ function getMoveTaskOverlayTemplate() {
   return `
     <div id="move_task_overlay" class="move-task-overlay" onclick="overlayProtection(event)">
       <h3>Move to</h3>
-      <button id="move_to_to_do" class="move-task-btn" onclick="moveTo('todo'); closeMoveTaskOverlay()">
+      <button id="move_to_to_do" class="move-task-btn" onclick="moveTo('todo'); closeMoveToOverlay()">
         <img src="./assets/svg/move_to_arrow_upward.svg" alt="move_to_arrow_upward">
         <span class="move-task-span">To Do</span>
       </button>
-      <button id="move_to_progress" class="move-task-btn" onclick="moveTo('progress'); closeMoveTaskOverlay()">
+      <button id="move_to_progress" class="move-task-btn" onclick="moveTo('progress'); closeMoveToOverlay()">
         <img src="./assets/svg/move_to_arrow_downward.svg" alt="move_to_arrow_downward">
         <span class="move-task-span">In Progress</span>
       </button>
-      <button id="move_to_feedback" class="move-task-btn" onclick="moveTo('feedback'); closeMoveTaskOverlay()">
+      <button id="move_to_feedback" class="move-task-btn" onclick="moveTo('feedback'); closeMoveToOverlay()">
         <img src="./assets/svg/move_to_arrow_downward.svg" alt="move_to_arrow_downward">
         <span class="move-task-span">Feedback</span>
       </button>
-      <button id="move_to_done" class="move-task-btn" onclick="moveTo('done'); closeMoveTaskOverlay()">
+      <button id="move_to_done" class="move-task-btn" onclick="moveTo('done'); closeMoveToOverlay()">
         <img src="./assets/svg/move_to_arrow_downward.svg" alt="move_to_arrow_downward">
         <span class="move-task-span">Done</span>
       </button>
